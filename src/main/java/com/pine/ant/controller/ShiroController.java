da93627950b6899141f0446b7d5aa87af2dffd06
shipei315 <shipei315@126.com>
2017-03-05 09:54:10 +0800

    测试github

--- src/main/java/com/pine/ant/controller/ShiroController.java
+++ src/main/java/com/pine/ant/controller/ShiroController.java
@@ -51,11 +51,13 @@ public class ShiroController {
         //获取当前的Subject  
         Subject currentUser = SecurityUtils.getSubject();  
         try {  
+            
             //在调用了login方法后,SecurityManager会收到AuthenticationToken,并将其发送给已配置的Realm执行必须的认证检查  
             //每个Realm都能在必要时对提交的AuthenticationTokens作出反应  
             //所以这一步在调用login(token)方法时,它会走到MyRealm.doGetAuthenticationInfo()方法中,具体验证方式详见此方法  
             logger.info("对用户[" + username + "]进行登录验证..验证开始");  
             currentUser.login(token); 
+            
             currentUser.hasRole("normal");
             logger.info("对用户[" + username + "]进行登录验证..验证通过");  
         }catch(UnknownAccountException uae){  
